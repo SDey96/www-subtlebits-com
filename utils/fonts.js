1133cd533995f60f6d4ed3f384fbc0fe9f29889e
Dino Hamzic <dino.hamzic@pm.me>
2018-11-24 00:17:39 +0100

    Create utility for font definitions

---
+++ utils/fonts.js
@@ -0,0 +1,15 @@
+const sailec = (
+  `"Sailec", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", ` +
+  `"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", ` +
+  `sans-serif`
+)
+
+const apercu = (
+  `"Apercu", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", ` +
+  `"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", ` +
+  `sans-serif`
+)
+
+const apercuMono = `"Apercu Mono", "Monaco", "Lucida Console", monospace`
+
+export { apercu, apercuMono, sailec }
