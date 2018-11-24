4da7fd8c2765a32d34cdcab47a41dda7ac8a3a06
Dino Hamzic <dino.hamzic@pm.me>
2018-11-24 18:20:41 +0100

    Fix Next.js console warning (`<title>` in `_document.js` not allowed)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import App, { Container } from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 
 import 'normalize.css'
 import '../static/css/global.css'
+
+const description = (
+  `Dino is a frontend engineer based in Sarajevo with over five years of software development experience. He is ` +
+  `currently developing applications using React, Next.js, TypeScript, D3.js, HTML and CSS`
+)
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -20,6 +26,12 @@ class MyApp extends App {
 
     return (
       <Container>
+        <Head>
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <title>Dino Hamzić &mdash; Software Engineer</title>
+          <meta name="description" content={description} />
+        </Head>
         <Component {...pageProps} />
       </Container>
     )
